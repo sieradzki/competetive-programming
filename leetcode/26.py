20e687cf63ef0b3167abfc585b04bb68d400867c
Mateusz Sieradzki <35726839+sieradzki@users.noreply.github.com>
2024-02-16 09:08:19 +0100

    Create 26.py

---
+++ leetcode/26.py
@@ -0,0 +1,7 @@
+class Solution:
+  def removeDuplicates(self, nums: List[int]) -> int:
+    for i in range(len(nums) - 1, 0, -1):
+      if i > 0 and nums[i] == nums[i - 1]:
+        nums.pop(i)
+
+    return len(nums)
